cbbcd3939f97809d0d72d60048aecb7a74d2547e
Joan Martínez Serres <hola@112books.eu>
2026-03-31 20:21:23 +0200

    feat: analytics amb browsers, systems, sizes, locations

--- scripts/process-analytics.py
+++ scripts/process-analytics.py
@@ -6,7 +6,8 @@ raw_file, out_file, start, end = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv
 with open(raw_file) as f:
     data = json.load(f)
 
-hits = data.get('hits', [])
+# raw_file conté tot el JSON amb hits + stats secundàries
+hits = data.get('hits_data', {}).get('hits', [])
 sections = ['about', 'the-band', 'music', 'concerts', 'videos', 'photos', 'contact']
 langs = ['ca', 'es', 'en']
 
@@ -29,12 +30,31 @@ for h in hits:
     if section in sections:
         by_section[section] += count
 
+def parse_stats(key):
+    return [{'id': s['id'], 'name': s.get('name') or s['id'], 'count': s['count']}
+            for s in data.get(key, {}).get('stats', [])]
+
+size_names = {
+    'phone': 'Mòbil',
+    'tablet': 'Tauleta',
+    'desktop': 'Escriptori',
+    'desktophd': 'Escriptori HD',
+    'larger': 'Pantalla gran'
+}
+
+sizes_raw = parse_stats('sizes')
+sizes = [{'id': s['id'], 'name': size_names.get(s['id'], s['id']), 'count': s['count']} for s in sizes_raw]
+
 result = {
     "generated": datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
     "period": {"start": start, "end": end},
     "total": total,
     "by_lang": by_lang,
-    "by_section": by_section
+    "by_section": by_section,
+    "browsers": parse_stats('browsers'),
+    "systems": parse_stats('systems'),
+    "sizes": sizes,
+    "locations": parse_stats('locations')
 }
 
 with open(out_file, 'w') as f:
